3e181240e4278dba79b1df6068917f490516cbfb
Luiz Fellipe Carneiro <fellipec@outlook.com>
2016-05-10 18:45:36 -0300

    Tratamento de exceções para o tweet

--- shakeasperepi.py
+++ shakeasperepi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #! python3
 
 #The pg100.txt file can be downloaded in Project Gutenberg page at https://www.gutenberg.org/ebooks/100.txt.utf-8
@@ -36,58 +35,19 @@ lastline = 0
 stopedat = where_stop()
 print("Interrompido na linha:" , stopedat)
 
+continuar = True
 
 for line in f:
     lastline += 1
     if not(line.strip() == ""):
         if lastline > stopedat:                  
             print(line.strip())
-            api.update_status(line.strip())
-            remember_where_stop(lastline)
-            time.sleep(180) 
+	    while continuar:
+	    	try:
+			api.update_status(line.strip())
+            		remember_where_stop(lastline)
+			continuar = False
+		except:
+			continuar = True
+            time.sleep(45) 
             
-=======
-#! python3
-import tweepy
-import time
-
-def remember_where_stop(linenumber):
-    r = open("remember.txt","w")
-    r.write(str(linenumber))
-    r.close
-    
-def where_stop():
-    try:
-        r = open("remember.txt","r")
-        ll = int(r.readline().strip())
-        r.close
-        return ll
-    except:
-        return 0
-
-consumer_key="[INSERT YOUR DATA HERE]"
-consumer_secret="[INSERT YOUR DATA HERE]"
-access_token_key="[INSERT YOUR DATA HERE]"
-access_token_secret="[INSERT YOUR DATA HERE]"
-
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token_key,access_token_secret)
-
-api = tweepy.API(auth)
-
-f = open("pg100.txt")
-lastline = 0
-stopedat = where_stop()
-print("Interrompido na linha:" , stopedat)
-
-
-for line in f:
-    lastline += 1
-    if not(line.strip() == ""):
-        if lastline > stopedat:                  
-            print(line.strip())
-            api.update_status(line.strip())
-            remember_where_stop(lastline)
-            time.sleep(180) 
-            
->>>>>>> origin/master
--- shakeasperepi_gettoken.py
+++ shakeasperepi_gettoken.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #! python3
 
 #Use this file to get the Twitter Access Tokens for OAuthHandler
@@ -29,32 +28,3 @@ except tweepy.TweepError:
 print(auth.access_token)
 print(auth.access_token_secret)
 
-
-=======
-import tweepy
-
-consumer_key="[INSERT YOUR DATA HERE]"
-consumer_secret="[INSERT YOUR DATA HERE]"
-
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-
-
-try:
-    redirect_url = auth.get_authorization_url()
-except tweepy.TweepError:
-    print("Failed to get request token")
- 
-print(redirect_url)    
-
-verifier = input('Verifier:')
-
-try:
-    auth.get_access_token(verifier)
-except tweepy.TweepError:
-    print("Failed to get request token")
-
-print(auth.access_token)
-print(auth.access_token_secret)
-
-
->>>>>>> origin/master
