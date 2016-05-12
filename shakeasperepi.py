f196efd1bc4ef9ba3f9d00beee211f183e9d0c18
Luiz Fellipe Carneiro <fellipec@outlook.com>
2016-05-12 14:23:07 -0300

    Melhoria no tratamento de erros e LOG

--- shakeasperepi.py
+++ shakeasperepi.py
@@ -1,10 +1,10 @@
 #! python3
+# coding=UTF-8
+import tweepy
+import time
 
 #The pg100.txt file can be downloaded in Project Gutenberg page at https://www.gutenberg.org/ebooks/100.txt.utf-8
 #William Shakespeake's works are now public domain. 
-
-import tweepy
-import time
 
 def remember_where_stop(linenumber):
     r = open("remember.txt","w")
@@ -19,6 +19,14 @@ def where_stop():
         return ll
     except:
         return 0
+        
+def dolog(logtext):
+    try:
+        l = open("logshakespeare.txt","a")
+        l.write(logtext + "\n")
+        l.close
+    except:
+        print("Erro gravando log", logtext)
 
 consumer_key="[INSERT YOUR DATA HERE]"
 consumer_secret="[INSERT YOUR DATA HERE]"
@@ -33,20 +41,27 @@ api = tweepy.API(auth)
 f = open("pg100.txt")
 lastline = 0
 stopedat = where_stop()
-print("Interrompido na linha:" , stopedat)
+dolog('Interrompido na linha: ' + str(stopedat))
 
 for line in f:
     lastline += 1
     if not(line.strip() == ""):
-        if lastline > stopedat:                  
-            print(line.strip())
+        if lastline > stopedat:
+            dolog(line.strip() + " " + str(lastline))
             continuar = True
             while continuar:
-                 try:
-                      api.update_status(line.strip())
-                      remember_where_stop(lastline)
-                      continuar = False
-                 except:
-                      continuar = True
-            time.sleep(45) 
-            
+                try:
+                    api.update_status(line.strip())
+                    remember_where_stop(lastline)
+                    continuar = False
+                except tweepy.error.TweepError as e:
+                    dolog('Erro ao tuitar:' + e)                    
+                    if e.api_code == 187:
+                        continuar = False
+                        dolog('Tuíte duplicado, passando para o próximo')
+                    else:
+                        continuar = True
+                        dolog ('Aguardando para tentar novamente')
+                        time.sleep(180)
+                time.sleep(45) 
+                    
